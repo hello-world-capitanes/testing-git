5baeffb960f83507b7913eea725d970144eba272
Carlos Moreno <carlosmz@devanddel.com>
2022-03-01 02:01:45 +0100

    Carga inicial del proyecto de ejemplo.

---
+++ main.js
@@ -0,0 +1,46 @@
+function suma(a, b) {
+    return a + b;
+}
+
+function multiplica(a, b) {
+    return a * b;
+}
+
+function resta(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function eleva(a, b) {
+   return Math.pow(a,b);
+}
+
+function raiz(a) {
+   return Math.sqrt(a)
+}
+
+function maximo(a, b){
+   return Math.max(a, b);
+}
+
+function minimo(a, b){
+    return Math.min(a, b);
+}
+
+function redondea(a) {
+    return Math.round(a);
+}
+
+console.log (suma(4,3));
+console.log (multiplica(4,3));
+console.log (resta(4,3));
+console.log (divide(4,3));
+console.log (eleva(4,3));
+console.log (raiz(4));
+console.log (maximo(4,3));
+console.log (minimo(4,3));
+console.log (redondea(divide(4,3)));
+
