068d47cd7c99f355b4182a3b8e93222b0757e499
Javier Roldan <javier.roldan@babelgroup.com>
2023-02-15 13:16:11 +0100

    scrollbar_func
    void scrol(){

--- GestionUsuarios.java
+++ GestionUsuarios.java
@@ -1,1 +1,28 @@
 console.log("ola");
+
+
+void scrol(){
+    <html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+</head>
+
+<body>
+<div id="Layer1" style="width:100px; height:115px; overflow: scroll;">Aquí saldrá un scroll<br>
+<br>
+sólo<br>
+dale<br>
+interlineados<br>
+simple<br>
+no ?
+</div>
+
+</body>
+</html>
+
+
+
+//Fuente: https://www.ejemplode.com/18-css/642-ejemplo_de_scroll_en_div_overflow.html#ixzz7tO5Jv2Pm
+}
