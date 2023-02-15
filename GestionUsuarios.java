eda2946e4431844163b1a77f34aa398957ac9e17
Andres Guijarro <andres.guijarro@babelgroup.com>
2023-02-15 13:16:16 +0100

    modificaciones boton y formulario

--- GestionUsuarios.java
+++ GestionUsuarios.java
@@ -1,1 +1,13 @@
-console.log("ola");
+function boton(a, b, c) {
+
+
+    console.log("Funcion que presenta un boton");
+
+}
+
+function validaformulario(a, b, c) {
+
+
+    console.log("Funcion que valida el formulario");
+
+}
